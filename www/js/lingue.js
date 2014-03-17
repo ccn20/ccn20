43dddbe78ba4c96c626b0ca0bc515feadcdaaf44
ccn20 <lorenzoonofrio@tin.it>
2014-03-17 15:45:53 +0100

    test

---
+++ www/js/lingue.js
@@ -0,0 +1,33 @@
+	{
+	"italiano": {
+		"idLingua": "1",
+		"codiceLingua": "it",
+		"nomeLingua": "Italiano",
+        "Categorie": "Categorie",
+		"filtra": "filtra",
+		"indietro": "indietro",
+		"ordina": "ordina",
+		"salta": "salta"
+    },
+	"inglese": {
+		"idLingua": "2",
+		"codiceLingua": "en",
+        "nomeLingua": "English",
+		"Categorie": "Categories",
+		"filtra": "filter",
+		"indietro": "back",
+		"ordina": "sort",
+		"salta": "skip"
+		
+    },
+	"francese": {
+		"idLingua": "3",
+		"codiceLingua": "fr",
+        "nomeLingua": "Francaise",
+		"Categorie": "Categorìes",
+		"filtra": "filtre",
+		"indietro": "arriere",
+		"ordina": "sortre"
+		
+    }
+	}

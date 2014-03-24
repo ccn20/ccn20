55494a93fc63af65efa7252506d3ac0295f4bc39
ccn20 <lorenzoonofrio@tin.it>
2014-03-24 09:41:05 +0100

    test

---
+++ www/gitaJSON.js
@@ -0,0 +1,48 @@
+[
+    {
+        "nomeEsercizio": "Esselunga",
+        "Motivi": {
+            "1": "comprare bottiglia di vino",
+            "2": "fare la spesa"
+        }
+    },
+	{
+        "nomeEsercizio": "Obi",
+        "Motivi": {
+            "1": "cadere in una truffa"
+        }
+    },
+	{
+        "nomeEsercizio": "Lidl",
+        "Motivi": {
+            "1": "trovare piante strane",
+            "2": "trovare cazzate nei cestoni",
+			"3": "comprare dolci e aggravare il tuo diabete"
+        }
+    },
+	{
+        "nomeEsercizio": "Aldi",
+        "Motivi": {
+            "1": "comprare dolci e aggravare il tuo diabete"
+        }
+    },
+	{
+        "nomeEsercizio": "Feltrinelli",
+        "Motivi": {
+            "1": "comprare il buono per gli ebook",
+            "2": "far una passeggiata"
+        }
+    },
+	{
+        "nomeEsercizio": "Cip Garden",
+        "Motivi": {
+            "1": "comprare piante aromatiche per il balcone"
+        }
+    },
+	{
+        "nomeEsercizio": "casa mia",
+        "Motivi": {
+            "1": "seminare il basilico",
+        }
+    }
+]

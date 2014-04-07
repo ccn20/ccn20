80d69b40a934e77377c22846b71408bc9b0d2b40
ccn20 <lorenzoonofrio@tin.it>
2014-04-07 15:50:55 +0200

    test

---
+++ www/js/16.js
@@ -0,0 +1,61 @@
+[
+    {
+        "idEsercizio": "157",
+        "NomeEsercizio": "Enoteca da Gigi",
+        "Indirizzo": "via Bernardino Luini 48",
+        "Citta": "Como",
+        "Cap": "22100",
+        "Latitudine": "45.809857200000",
+        "Longitudine": "9.083437300000",
+        "Orario": {
+            "lunedi": "15.00-18.00",
+            "martedi": "9.30-18.00",
+            "mercoledi": "15.00-18.00",
+            "giovedi": "9.30-18.00",
+            "venerdi": "9.30-18.00",
+            "sabato": "9.30-18.00",
+            "domenica": "9.30-18.00"
+        },
+        "Website": "http://www.enotecagigi.com",
+        "Email": "info@enotecagigi.com",
+        "Statistiche": "si",
+        "Distanze": {
+            "34": {
+                "idEsercizio": "34",
+                "nomeEsercizio": "Moglia",
+                "distance": "79",
+                "time": "57"
+            },
+            "35": {
+                "idEsercizio": "35",
+                "nomeEsercizio": "Seveso",
+                "distance": "118",
+                "time": "85"
+            },
+            "147": {
+                "idEsercizio": "147",
+                "nomeEsercizio": "Viamaestra",
+                "distance": "29",
+                "time": "21"
+            },
+            "151": {
+                "idEsercizio": "151",
+                "nomeEsercizio": "Hops!",
+                "distance": "44",
+                "time": "32"
+            },
+            "152": {
+                "idEsercizio": "152",
+                "nomeEsercizio": "Filippo Caimi",
+                "distance": "138",
+                "time": "99"
+            },
+            "153": {
+                "idEsercizio": "153",
+                "nomeEsercizio": "Gloook",
+                "distance": "91",
+                "time": "66"
+            }
+        }
+    }
+]
